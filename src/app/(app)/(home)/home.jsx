cec016d53c027e5215d39ed2207d3ba3015dbdbf
wethinkcode <wkubheka023@student.wethinkcode.co.za>
2024-03-20 13:54:26 +0200

    working on filter buttons

--- src/app/(app)/(home)/home.jsx
+++ src/app/(app)/(home)/home.jsx
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { FontAwesome5 } from "@expo/vector-icons";
+import SearchInput from "../../../components/searchInput";
 
 const Home = () => {
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
+    <SafeAreaView className="flex-1 bg-stone-950">
+      <View>
+        <View className="flex-row self-end m-8">
+          <TouchableOpacity className=" bg-neutral-800  p-3 rounded-xl">
+            <FontAwesome5 name="user-alt" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+        <SearchInput />
+      </View>
     </SafeAreaView>
   );
 };
---
+++ src/components/searchInput.jsx
@@ -0,0 +1,28 @@
+import { SearchBar } from "@rneui/themed";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  return (
+    <SearchBar
+      placeholder="Type Here..."
+      onChangeText={setSearch}
+      value={search}
+      containerStyle={{
+        width: "90%",
+        alignSelf: "center",
+        backgroundColor: "transparent",
+      }}
+      inputContainerStyle={{
+        backgroundColor: "transparent",
+        borderWidth: 1,
+        borderColor: "rgba(94, 94, 94, 0.8)",
+      }}
+    />
+  );
+};
+
+export default SearchInput;
+
+const styles = StyleSheet.create({});
