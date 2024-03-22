3f5a630d4220ae80bfa052d635c094903a692dbe
wkubheka023 <wkubheka023@student.wethinkcode.co.za>
2024-03-22 15:07:32 +0200

    circular progress

---
+++ .history/src/app/(app)/(home)/AddNote_20240322123748.jsx
@@ -0,0 +1,189 @@
+import {
+  Alert,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+} from "react-native";
+import { useState } from "react";
+import { Stack } from "expo-router";
+import { Input } from "@rneui/themed";
+import DropDownPicker from "react-native-dropdown-picker";
+import { auth } from "../../../../firebase";
+import { createUserTodoNote } from "../../../backend/firestore";
+import { FontAwesome6 } from "@expo/vector-icons";
+
+const AddNote = () => {
+  const [title, setTitle] = useState("");
+  const [deadline, setDeadline] = useState("");
+  const [description, setDescription] = useState("");
+  const [subProblem, setSubProblem] = useState("");
+  const [subProblems, setSubProblems] = useState([]);
+  const [teamEmail, setTeamEmail] = useState("");
+  const [team, setTeam] = useState([auth.currentUser?.email]);
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    { label: "Personal", value: "personal" },
+    { label: "Work", value: "work" },
+    { label: "Team", value: "team" },
+  ]);
+  const formFilled = () => {
+    if (
+      title !== "" &&
+      deadline !== "" &&
+      description !== "" &&
+      value !== null
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  const addSubProblem = () => {
+    if (subProblem === "") {
+      setSubProblem("");
+      Alert.alert("Can't add an Empty Sub Problem ");
+      return;
+    }
+    if (!formFilled()) {
+      setSubProblem("");
+      Alert.alert("Please fill in All fields first");
+      return;
+    }
+    setSubProblems([...subProblems, subProblem]);
+    setSubProblem("");
+  };
+
+  return (
+    <View className=" bg-stone-950 flex-1">
+      <ScrollView className="flex-1 px-5 gap-10">
+        <Stack.Screen
+          options={{
+            headerShown: true,
+            headerTitle: "Add A Note",
+            headerTitleStyle: {
+              color: "white",
+              fontFamily: "Sofia",
+              fontSize: 16,
+            },
+            headerStyle: {
+              backgroundColor: "#0c0a09",
+            },
+          }}
+        />
+
+        <View>
+          <Text className=" text-neutral-500" style={{ fontFamily: "Sofia" }}>
+            Title
+          </Text>
+          <TextInput
+            className=" py-2 px-5 border border-neutral-500 rounded-md mt-2"
+            style={{ fontFamily: "SofiaLight", fontSize: 15 }}
+            placeholderTextColor={"#5E5E5E"}
+            value={title}
+            onChangeText={setTitle}
+            placeholder="Add to do Title"
+          />
+        </View>
+        <View>
+          <Text
+            className=" text-neutral-500 pb-3"
+            style={{ fontFamily: "Sofia" }}
+          >
+            type
+          </Text>
+          <DropDownPicker
+            placeholder=""
+            open={open}
+            value={value}
+            items={items}
+            setOpen={setOpen}
+            setValue={setValue}
+            setItems={setItems}
+          />
+        </View>
+        {value === "team" && (
+          <View>
+            <Text className=" text-neutral-500" style={{ fontFamily: "Sofia" }}>
+              Invite
+            </Text>
+            <TextInput
+              className=" py-2 px-5 border border-neutral-500 rounded-md mt-2"
+              style={{ fontFamily: "SofiaLight", fontSize: 15 }}
+              placeholderTextColor={"#5E5E5E"}
+              value={teamEmail}
+              onChangeText={setTeamEmail}
+              placeholder="TeamMate email"
+            />
+            <TouchableOpacity
+              onPress={() => {
+                if (teamEmail.includes("@")) {
+                  setTeam([...team, teamEmail]);
+                }
+              }}
+              className="w-15 bg-red-50 h-10 justify-center items-center"
+            >
+              <Text className="  bg-neutral-800 text-5xl text-white">+</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+        <View>
+          <Text className=" text-neutral-500" style={{ fontFamily: "Sofia" }}>
+            Deadline
+          </Text>
+          <TextInput
+            className=" py-2 px-5 border border-neutral-500 rounded-md mt-2"
+            style={{ fontFamily: "SofiaLight", fontSize: 15 }}
+            placeholderTextColor={"#5E5E5E"}
+            value={deadline}
+            onChangeText={setDeadline}
+            placeholder="deadline"
+          />
+        </View>
+        <View>
+          <Text className=" text-neutral-500" style={{ fontFamily: "Sofia" }}>
+            Description
+          </Text>
+          <TextInput
+            className=" py-2 px-5 border border-neutral-500 rounded-md mt-2"
+            style={{ fontFamily: "SofiaLight", fontSize: 15 }}
+            placeholderTextColor={"#5E5E5E"}
+            value={description}
+            onChangeText={setDescription}
+            placeholder="Add To Do Description"
+          />
+        </View>
+        <View>
+          <Text className=" text-neutral-500" style={{ fontFamily: "Sofia" }}>
+            Sub Problem
+          </Text>
+          <View className=" px-5 flex-row border border-neutral-500 rounded-md mt-2 justify-between items-center">
+            <TextInput
+              className=" py-2  border "
+              style={{ fontFamily: "SofiaLight", fontSize: 15 }}
+              placeholderTextColor={"#5E5E5E"}
+              value={subProblem}
+              onChangeText={setSubProblem}
+              placeholder="Add Sub Problems"
+            />
+            <TouchableOpacity onPress={addSubProblem}>
+              <FontAwesome6 name="add" size={24} color="#262626" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+      <TouchableOpacity
+        onPress={() =>
+          createUserTodoNote(title, deadline, team, value, subProblem)
+        }
+        className=" absolute bottom-5 right-5 w-12 h-12 bg-neutral-800 justify-center items-center rounded-md"
+      >
+        <FontAwesome6 name="check" size={24} color="white" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default AddNote;
---
+++ .history/src/components/todoCard_20240322142751.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Dimensions,
+} from "react-native";
+import dayjs from "dayjs";
+import { AnimatedCircularProgress } from "react-native-circular-progress";
+
+// import CircularProgress from "react-native-circular-progress-indicator";
+
+const TodoCard = ({ todo }) => {
+  const percentComplete = () => {
+    if (todo.subProblem > 0) {
+      return Math.floor((todo.subProblemCompleted / todo.subProblem) * 100);
+    } else {
+      return todo.subProblemCompleted === 0 ? 0 : 100;
+    }
+  };
+
+  return (
+    <TouchableOpacity activeOpacity={0.05} style={styles.container}>
+      <View style={styles.leftContainer}>
+        <Text style={styles.heading}>{todo.type}</Text>
+        <Text style={[styles.text, { fontFamily: "SofiaLight" }]}>
+          {todo.title}
+        </Text>
+      </View>
+      <View style={styles.rightContainer}>
+        <View
+          style={{
+            alignItems: "center",
+            flex: 1,
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={[styles.text, { fontFamily: "SofiaBold" }]}>
+            {dayjs(todo.deadline).format("mm/d")}
+          </Text>
+          <View style={styles.circle}>
+            <AnimatedCircularProgress
+              size={90}
+              width={10}
+              fill={percentComplete()}
+              tintColor="#00e0ff"
+              onAnimationComplete={() => console.log("onAnimationComplete")}
+              backgroundColor="#3E3E3E"
+            >
+              {(fill) => <Text>{fill}</Text>}
+            </AnimatedCircularProgress>
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default TodoCard;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 20,
+    backgroundColor: "rgba(36,36,36,0.5)",
+    opacity: 0.5,
+    height: Dimensions.get("window").width / 2.5,
+    padding: 20,
+    borderRadius: 20,
+  },
+  text: {
+    color: "#A6A6A6",
+  },
+  heading: {
+    fontFamily: "Irish",
+    fontSize: 20,
+    color: "white",
+  },
+  leftContainer: {
+    justifyContent: "space-around",
+    flex: 2,
+  },
+  rightContainer: {
+    flex: 1,
+    alignItems: "flex-end",
+    justifyContent: "space-between",
+  },
+  circle: {
+    // backgroundColor: colors.circleBackground,
+    width: 70,
+    aspectRatio: 1,
+    borderRadius: 999,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
