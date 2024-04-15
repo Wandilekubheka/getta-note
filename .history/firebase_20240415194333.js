5bda6e51cf73d4c1fb658835de17ccda40260377
wkubheka023 <wkubheka023@student.wethinkcode.co.za>
2024-04-15 19:45:48 +0200

    env

--- .history/firebase_20240415194333.js
+++ .history/firebase_20240415194333.js
@@ -5,14 +5,13 @@ import {
   getReactNativePersistence,
 } from "firebase/auth/react-native";
 import { initializeFirestore } from "firebase/firestore";
-
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API,
-  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: EXPO_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_APP_ID,
+  apiKey: ,
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 let app;
